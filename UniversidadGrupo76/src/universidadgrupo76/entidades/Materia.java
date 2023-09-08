3b33780326dd5eff9e33197f0d60f27b90a20c4a
futures14 <122988205+futures14@users.noreply.github.com>
2023-09-08 11:53:41 -0300

    clase Materia
    su suma la clase materia dentro de entidades con  sus constructores y los getter y setter

--- UniversidadGrupo76/src/universidadgrupo76/entidades/Materia.java
+++ UniversidadGrupo76/src/universidadgrupo76/entidades/Materia.java
@@ -10,5 +10,64 @@ package universidadgrupo76.entidades;
  * @author Fernandez
  */
 public class Materia {
+    private int idMateria;
+    private String nombre;
+    private int ano ;
+    private boolean estado;
+
+    public Materia() {
+    }
+
+    public Materia(int idMateria, String nombre, int ano, boolean estado) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.ano = ano;
+        this.estado = estado;
+    }
+
+    public Materia(String nombre, int ano, boolean estado) {
+        this.nombre = nombre;
+        this.ano = ano;
+        this.estado = estado;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", ano=" + ano + ", estado=" + estado + '}';
+    }
+    
+   
     
 }
